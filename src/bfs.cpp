d340a1b998c6011e572a39b62c6756e0136d7b1c
Jiyang Xu <92758419+jiyangxxu@users.noreply.github.com>
2022-11-30 17:52:06 -0600

    Add files via upload
    finish two bfs functions

--- src/bfs.cpp
+++ src/bfs.cpp
@@ -20,9 +20,9 @@ vector<string> BFS::traverseAll(int sourceAP) {
 
     while (!(q.empty())) {
         currentAP = q.front();
-        ret.push_back(graph_.getAirportNameByID(sourceAP));
+        ret.push_back(graph_.getAirportNameByID(currentAP));
 
-        for (auto ap : graph_.adjacent(sourceAP)) {
+        for (auto ap : graph_.adjacent(currentAP)) {
             if (labels[ap] == false) { // vertex is unexplored
                 labels[ap] = true;
                 q.push(ap);
@@ -34,5 +34,34 @@ vector<string> BFS::traverseAll(int sourceAP) {
 }
 
 vector<string> BFS::traverseByDest(int sourceAP, int destAP) {
-    return nullptr;
+    vector<string> ret;
+    queue<int> q;
+    // 14110: because there is 14110 airports in airport dataset
+    vector<bool> labels(14110, false);
+
+    q.push(sourceAP);
+    labels[sourceAP] = true; // label sourceAP (starting point) as visited
+
+    int currentAP = sourceAP; // copy of the input id
+    labels[currentAP] = true;
+
+    while (!(q.empty())) {
+        currentAP = q.front();
+
+        if (currentAP == destAP) { // check if we reach the destination airport
+            break;
+            ret.push_back(graph_.getAirportNameByID(currentAP));
+        }
+
+        ret.push_back(graph_.getAirportNameByID(currentAP));
+
+        for (auto ap : graph_.adjacent(sourceAP)) {
+            if (labels[ap] == false) { // vertex is unexplored
+                labels[ap] = true;
+                q.push(ap);
+            }
+        }
+        q.pop();
+    }
+    return ret;
 }
