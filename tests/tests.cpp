f1a99aa70658b56493b8160224bdca93c3194f97
Bingheng Li <70342993+jasonliiiii@users.noreply.github.com>
2022-12-08 13:57:35 -0600

    Add files via upload

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -97,5 +97,45 @@ TEST_CASE("BFS", "[weight=1][timeout=30000]") {
   REQUIRE(path.size() == 3);
   REQUIRE(path.at(0) == "London Heathrow Airport");
   REQUIRE(path.at(1) == "Kugaaruk Airport");
-  REQUIRE(path.at(2) == "Hamburg Hbf");
+  REQUIRE(path.at(2) == "Pokhara Airport");
+}
+
+TEST_CASE("Find shortest path by distance", "[weight=1][timeout=30000]") {
+  Airport a1 = Airport(507, "London Heathrow Airport", "London", "United Kingdom", 51.4706, -0.461941);
+  Airport a2 = Airport(26,"Kugaaruk Airport","Pelly Bay","Canada",68.534401,-89.808098);
+  Airport a3 = Airport(3127,"Pokhara Airport","Pokhara","Nepal",28.200899124145508,83.98210144042969);
+  Airport a4 = Airport(8810,"Hamburg Hbf","Hamburg","Germany",53.552776,10.006683);
+  vector<Airport> airports = {a1, a2, a3, a4};
+  Graph g = Graph();
+  Route r1 = Route(507, 26, 1000); // a1 -> a2 by distance 1000
+  Route r2 = Route(26, 3127, 100); // a2 -> a3 by distance 100
+  Route r3 = Route(507, 8810, 2000); // a1 -> a4 by distance 2000
+  Route r4 = Route(8810, 3127, 1000); // a3 -> a4 by distance 1000
+  vector<Route> routes = {r1, r2, r3, r4};
+  g.setRelationMap(routes);
+  vector<int> path = g.dijkstras(507, 3127);
+  REQUIRE(path.size() == 3);
+  REQUIRE(path.at(0) == 507);
+  REQUIRE(path.at(1) == 26);
+  REQUIRE(path.at(2) == 3127);
+}
+
+TEST_CASE("Find strongly connected component", "[weight=1][timeout=30000]") {
+  Airport a1 = Airport(507, "London Heathrow Airport", "London", "United Kingdom", 51.4706, -0.461941);
+  Airport a2 = Airport(26,"Kugaaruk Airport","Pelly Bay","Canada",68.534401,-89.808098);
+  Airport a3 = Airport(3127,"Pokhara Airport","Pokhara","Nepal",28.200899124145508,83.98210144042969);
+  Airport a4 = Airport(8810,"Hamburg Hbf","Hamburg","Germany",53.552776,10.006683);
+  vector<Airport> airports = {a1, a2, a3, a4};
+  Graph g = Graph();
+  Route r1 = Route(507, 26, 1000); // a1 -> a2
+  Route r2 = Route(26, 3127, 100); // a2 -> a3 
+  Route r3 = Route(3127, 507, 2000); // a3 -> a1 --> stongly connected!
+  Route r4 = Route(26, 8810, 1000); // a2 -> a4
+  vector<Route> routes = {r1, r2, r3, r4};
+  g.setRelationMap(routes);
+  vector<int> path = g.kosaraju(507);
+  REQUIRE(path.size() == 3);
+  REQUIRE(path.at(0) == 507);
+  REQUIRE(path.at(1) == 26);
+  REQUIRE(path.at(2) == 3127);
 }
