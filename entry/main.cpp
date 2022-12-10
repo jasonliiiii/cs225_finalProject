3845470afa0c4956eb31bdf356e9c19fcb5b044a
Bingheng Li <70342993+jasonliiiii@users.noreply.github.com>
2022-12-10 15:50:01 -0600

    Add files via upload

--- entry/main.cpp
+++ entry/main.cpp
@@ -5,9 +5,15 @@
 
 #include "airport.h"
 #include "airport_graph.h"
+#include "route.h"
 
 using namespace std;
 
 int main() {
-    return 0;
+    Graph airport_web = Graph("/workspaces/cs225/release-f22/final_project/src/airports.dat.txt", 
+    "/workspaces/cs225/release-f22/final_project/src/routes.dat.txt");
+    map<int, Airport> m = airport_web.getVertices();
+    cout << "The size of the vertext is: " << m.size() << endl;
+    map<int, set<pair<int,double>>> r = airport_web.getRelatedAirports();
+    cout << "The size of the related map is: " << r.size() << endl;
 }
